98e38dc56fb4f769b8e85216eb9a613ba17e9089
Kapil Khubchandani <kapil.khubchndani@adek.gov.ae>
2023-09-10 18:55:56 +0400

    commit for testing theory with Member data

--- EmployeeManagement.Test/DataDrivenEmployeeServiceTests.cs
+++ EmployeeManagement.Test/DataDrivenEmployeeServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using EmployeeManagement.DataAccess.Entities;
 using EmployeeManagement.Test.Fixtures;
 using Xunit;
+using Xunit.Sdk;
 
 namespace EmployeeManagement.Test
 {
@@ -46,13 +47,55 @@ namespace EmployeeManagement.Test
 			Assert.False(internalEmployee.MinimumRaiseGiven);
 		}
 
+		public static IEnumerable<object[]> EmployeeTestDataForGiveRaise_WithProperty
+		{
+			get
+			{
+				return new List<object[]> {
+				 new object[] {100, true },
+				 new object[] {200, false }
+				};
+			}
+		}
+
+		public static IEnumerable<object[]> EmployeeTestDataForGiveRaise_WithMethod(int allowedData)
+		{
+			var list  = new List<object[]> {
+				 new object[] {100, true },
+				 new object[] {200, false }
+				};
+
+			return list.Take(allowedData);
+
+		}
+
+		[Theory]
+		//[MemberData(nameof(EmployeeTestDataForGiveRaise_WithProperty))]
+		//[MemberData(nameof(EmployeeTestDataForGiveRaise_WithMethod))]
+		[MemberData(nameof(DataDrivenEmployeeServiceTests.EmployeeTestDataForGiveRaise_WithMethod),
+			1,
+			MemberType = typeof(DataDrivenEmployeeServiceTests))
+		]
+		public async Task GiveRaise_RaiseGiven_EmployeeMinimumRaiseGivenMatchesValue(int raiseGiven, bool expectedValueForMinimumRaiseGiven)
+		{
+			var internalEmployee = new InternalEmployee(
+			"Brooklyn", "Cannon", 5, 3000, false, 1);
+
+			// Act 
+			await _employeeServiceFixture.EmployeeService
+				.GiveRaiseAsync(internalEmployee, raiseGiven);
+
+			// Assert
+			Assert.Equal(expectedValueForMinimumRaiseGiven, internalEmployee.MinimumRaiseGiven);
+		}
+
 
 		[Theory]
 		[InlineData("1fd115cf-f44c-4982-86bc-a8fe2e4ff83e")]
 		[InlineData("37e03ca7-c730-4351-834c-b66f280cdb01")]
 		public void CreateInternalEmployee_InternalEmployeeCreated_MustHaveAttendedSecondObligatoryCourse(Guid courseId)
 		{
-			
+
 			// Act
 			var internalEmployee = _employeeServiceFixture.EmployeeService.CreateInternalEmployee("Brooklyn", "Cannon");
 
